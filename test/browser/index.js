ae9ca05aeeff68314f7151efb1bac8dae026d296
Rutger Farry <rutgerfarry@gmail.com>
2017-04-26 20:19:13 -0700

    Ran Standard autofix, still a lot of warnings

--- test/browser/index.js
+++ test/browser/index.js
@@ -1,41 +1,41 @@
-var fs = require('fs');
+var fs = require('fs')
 
-var test = require('../')
-  , runTests = test.runTests
-  , load = test.load;
+var test = require('../'),
+  runTests = test.runTests,
+  load = test.load
 
-var express = require('express')
-  , app = express();
+var express = require('express'),
+  app = express()
 
-app.use(function(req, res, next) {
-  var setHeader = res.setHeader;
-  res.setHeader = function(name) {
+app.use(function (req, res, next) {
+  var setHeader = res.setHeader
+  res.setHeader = function (name) {
     switch (name) {
       case 'Cache-Control':
       case 'Last-Modified':
       case 'ETag':
-        return;
+        return
     }
-    return setHeader.apply(res, arguments);
-  };
-  next();
-});
+    return setHeader.apply(res, arguments)
+  }
+  next()
+})
 
-var dir = __dirname + '/../tests'
-  , files = {};
+var dir = __dirname + '/../tests',
+  files = {}
 
-app.get('/test.js', function(req, res, next) {
-  var test = fs.readFileSync(__dirname + '/test.js', 'utf8')
-    , files = load();
+app.get('/test.js', function (req, res, next) {
+  var test = fs.readFileSync(__dirname + '/test.js', 'utf8'),
+    files = load()
 
-  test = test.replace('__TESTS__', JSON.stringify(files));
-  test = test.replace('__MAIN__', runTests + '');
+  test = test.replace('__TESTS__', JSON.stringify(files))
+  test = test.replace('__MAIN__', runTests + '')
 
-  res.contentType('.js');
-  res.send(test);
-});
+  res.contentType('.js')
+  res.send(test)
+})
 
-app.use(express.static(__dirname + '/../../lib'));
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/../../lib'))
+app.use(express.static(__dirname))
 
-app.listen(8080);
+app.listen(8080)
