4a71e5ea2730c4d94f51a44b27da68268d5c7d22
Rutger Farry <rutgerfarry@gmail.com>
2017-04-29 14:37:05 -0700

    Basic tests run, but don’t pass options yet

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,18 +1,25 @@
 const fs = require('fs')
 const path = require('path')
+const marked = require('../')
 
-function load () {
+function files () {
   const absdir = __dirname
   const dirs = ['new', 'original'].map(testdir => path.join(absdir, testdir))
 
   const textfiles = dirs.map(dir => {
     return fs.readdirSync(dir)
              .filter(file => path.extname(file) === '.text')
+             .map(file => {
+               return { name: file, path: path.join(dir, file) }
+             })
   }).reduce((a, b) => a.concat(b))
 
   const htmlfiles = dirs.map(dir => {
     return fs.readdirSync(dir)
              .filter(file => path.extname(file) === '.html')
+             .map(file => {
+               return { name: file, path: path.join(dir, file) }
+             })
   }).reduce((a, b) => a.concat(b))
 
   return textfiles.map((_, i) => {
@@ -20,4 +27,12 @@ function load () {
   })
 }
 
-console.log(load())
+for (let file of files()) {
+  const textstring = fs.readFileSync(file.text.path, 'utf8')
+  const htmlstring = fs.readFileSync(file.html.path, 'utf8')
+
+  test(`${file.text.name} converts to ${file.html.name}`, () => {
+    const result = marked(textstring)
+    expect(result).toBe(htmlstring)
+  })
+}
